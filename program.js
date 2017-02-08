36e5b3859d631abd8b09315f46dc7452d6c46b45
Jose Torreblanca <josewhitetower@gmail.com>
2017-02-09 00:08:05 +0100

    my first async i/o

--- program.js
+++ program.js
@@ -10,8 +10,21 @@ for (var i = 2; i < array.length; i++) {
 console.log(total);*/
 
 //My first I/O
-var fs= require('fs');
+/*var fs= require('fs');
 var path= process.argv[2];
 var buffer = fs.readFileSync(path).toString();
 var array= buffer.split('\n');
-console.log(array.length - 1);
+console.log(array.length - 1);*/
+
+//My First Async I/O
+var fs= require('fs');
+var file = process.argv[2];
+
+var content ="";
+fs.readFile(file,function(err,data){
+  if(err) throw err;
+    var num =data.toString().split('\n').length -1;
+    console.log(num);
+})
+
+
