e1b2c3077efd7a9a4e4644c5d5c1a3635953e57e
Jose Torreblanca <josewhitetower@gmail.com>
2017-02-09 12:20:16 +0100

    LSFiltrado

--- program.js
+++ program.js
@@ -1,8 +1,8 @@
-/*//Hello World
-console.log("HELLO WORLD");
+//Hello World
+// console.log("HELLO WORLD");
 
 //Baby Steps
-var array=process.argv;
+/*var array=process.argv;
 var total=0;
 for (var i = 2; i < array.length; i++) {
   total += +array[i];
@@ -10,21 +10,36 @@ for (var i = 2; i < array.length; i++) {
 console.log(total);*/
 
 //My first I/O
-/*var fs= require('fs');
-var path= process.argv[2];
-var buffer = fs.readFileSync(path).toString();
-var array= buffer.split('\n');
-console.log(array.length - 1);*/
+// var fs= require('fs');
+// var path= process.argv[2];
+// var buffer = fs.readFileSync(path).toString();
+// var array= buffer.split('\n');
+// console.log(array.length - 1);
 
 //My First Async I/O
-var fs= require('fs');
+/*var fs= require('fs');
 var file = process.argv[2];
 
 var content ="";
-fs.readFile(file,function(err,data){
+fs.readFile(file,(err,data)=>{
   if(err) throw err;
     var num =data.toString().split('\n').length -1;
     console.log(num);
-})
+});*/
 
+//LSFiltrado
 
+	var fs= require('fs');
+	var p = require('path');
+	var path= process.argv[2];
+	var ext = '.' + process.argv[3];
+	 fs.readdir(path,(err,list)=>{
+	 	if (!err) {
+	 		for (var i = 0; i < list.length; i++) {
+	 		if (p.extname(list[i])===ext) {
+	 			console.log(list[i]);
+	 		}
+	 	}
+	 	}
+	 	
+	 });
